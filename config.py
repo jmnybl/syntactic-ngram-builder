ab762f2e12859e5c5b71c22bd972129a97e5684c
Jenna Kanerva <jmnybl@utu.fi>
2015-09-04 22:53:32 +0300

    clean config file

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 ### DEPENDENCY TYPES FOR EXTENDED NGRAMS ###
 
 # jump over prepositions and take the prep_obj instead
-ext_zero=u"prep".split() ## we don't have these in Finnish
+ext_zero=u"".split() ## we don't have these in Finnish
 
 # 'multiword expressions', include functional dependencies if head token is included (UD types: cc adpos case goeswith mwe cc:preconj preconj)
-ext_inc=u"cc adpos case goeswith mwe cc:preconj preconj".split() ## adpos is always included because it's the Finnish version of prep
+ext_inc=u"".split() ## adpos is always included because it's the Finnish version of prep
 
 #other functional dependencies (UD types: det ps complm prt neg aux auxpass mark compound:prt punct)
-ext_special=u"det ps complm prt neg aux auxpass mark compound:prt punct".split()
+ext_special=u"".split()
 
 # coordination types (UD types: conj)
-coords=u"conj".split()
+coords=u"".split()
 
 # coordinating conjunctions (UD types: cc, cc:preconj, preconj)
-coord_conjs=u"cc cc:preconj preconj".split()
+coord_conjs=u"".split()
